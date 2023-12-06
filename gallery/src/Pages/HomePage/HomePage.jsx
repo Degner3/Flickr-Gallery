a8fea4daf5f8bc9b99b06338b4fa17987f92607e
Degner3 <niklaschristiansen@live.dk>
2023-12-06 12:09:38 +0100

    console fix

--- gallery/src/Pages/HomePage/HomePage.jsx
+++ gallery/src/Pages/HomePage/HomePage.jsx
@@ -12,7 +12,7 @@ export const HomePage = () => {
     `https://www.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=86ca0c4a6448e0bc7b5b0e77340bbcbf&gallery_id=72157646472797634&get_user_info=1&get_gallery_info=1&extras=description%2C+license%2C+date_upload%2C+date_taken%2C+owner_name%2C+icon_server%2C+original_format%2C+last_update%2C+geo%2C+tags%2C++views%2C+media%2C+path_alias%2C&format=json&nojsoncallback=1`
   );
 
-  console.log(data.photos.photo);
+  // console.log(data.photos.photo);
 
   // if (data && data.photos && data.photos.photo) {
   //   data.photos.photo.forEach(photo => {
